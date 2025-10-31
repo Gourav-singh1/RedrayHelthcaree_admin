d2d8f39098b1e05e4554f25736d896ba74c53d95
Gourav-singh1 <singhgourav13326@gmail.com>
2025-10-31 20:25:34 +0530

    change backend url

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 // const BASE_URL = "https://namaapi.appdeft.biz/api/admin";
 // const HOST = "https://namaapi.appdeft.biz/";
 
-// const BASE_URL = "http://localhost:5000/api";
-const BASE_URL = "https://redrayhelthcaree.onrender.com/api";
+const BASE_URL = "http://localhost:5000/api";
+// const BASE_URL = "https://redrayhelthcaree.onrender.com/api";
 
 // const BASE_URL = "https://api.namameditation.com/api/admin";
 const HOST = "https://api.namameditation.com/";
